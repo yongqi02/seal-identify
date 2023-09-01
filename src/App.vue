2dc88d15b06d690d6cd1b773456902ff5334a8b5
heyq <yq9932@qq.com>
2023-09-01 09:20:27 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { fabric } from 'fabric';
 import {onMounted, ref} from 'vue';
-import img from './assets/印章1.jpg';
-// import {read} from "fs";
 
 function convertTo2DArray(arr, size) {
 	const res = [];
@@ -45,22 +43,6 @@ function isColWhite(arr, colIndex) {
 		}
 	}
 	return true;
-}
-
-
-function download() {
-	const canvas = document.querySelector('#canvas');
-	const preview = document.querySelector('#preview');
-	preview.width = canvas.width;
-	preview.height = canvas.height;
-	const imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-	preview.getContext('2d').putImageData(imageData, 0, 0);
-	const link = document.createElement('a');
-	link.download = 'canvas.png';
-	// link.href = dataURL;
-	document.body.appendChild(link);
-	link.click();
-	document.body.removeChild(link);
 }
 
 const input = ref(null);
@@ -151,46 +133,6 @@ onMounted(() => {
 
 	});
 });
-
-// onMounted(() => {
-// 	new Promise(resolve => {
-// 		const canvas = new fabric.Canvas('canvas', {
-// 			width: 1000,
-// 			height: 1000,
-// 			background: '#EEE'
-// 		});
-// 		const image = new Image();
-// 		image.src = img;
-// 		image.onload = () => {
-// 			const i = new fabric.Image(image, {
-// 				scaleX: 0.5,
-// 				scaleY: 0.5,
-// 				angle: 0,
-// 				top: 0,
-// 				left: 0
-// 			});
-// 			i.filters.push(new fabric.Image.filters.Contrast({
-// 				contrast: 0.5
-// 			}));
-// 			i.filters.push(new fabric.Image.filters.RemoveColor({
-// 				distance: 0.5
-// 			}));
-// 			i.filters.push(new fabric.Image.filters.Saturation({
-// 				saturation: 1
-// 			}));
-// 			i.applyFilters();
-// 			canvas.add(i);
-// 			const result = canvas.toObject();
-// 			console.log(result);
-// 			resolve();
-// 		};
-// 	}).then(() => {
-// 		setTimeout(() => {
-// 			download();
-// 		}, 0);
-// 	});
-// });
-
 
 </script>
 
